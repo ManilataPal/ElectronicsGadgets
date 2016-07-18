99a420b3078a1699d9b8cb03209db0f6625caf62
ManilataPal <manilatapal348@gmail.com>
2016-07-18 17:38:02 +0530

    Electronics

---
+++ src/main/java/com/niit/electronics/controller/ElectronicsController.java
@@ -0,0 +1,14 @@
+package com.niit.electronics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ElectronicsController {
+
+	@RequestMapping("/")
+	public String homePage()
+	{
+		return "index";
+	}
+}
